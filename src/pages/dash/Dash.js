e0476fa806f4acbd62fecef7d5fd9f396fa4f676
allcantara <alcantara.desenvolvedor@gmail.com>
2020-02-29 17:40:52 -0300

    Foto do usuário na tela de inicio

--- src/pages/dash/Dash.js
+++ src/pages/dash/Dash.js
@@ -1,15 +1,61 @@
-import React from "react";
+// @ts-nocheck
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWindowClose } from '@fortawesome/free-solid-svg-icons';
+import { useSnackbar } from 'notistack';
+import typeMessage from "../../constants/typeMessage";
+import api from '../../services/api';
 import "./Dash.css";
 
 export default ({ history, location }) => {
-  const { user } = location.state;
-  console.log(user)
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+    getUser()
+  }, [])
+
+  const getUser = async () => {
+    try {
+      const { user: User } = location.state
+      const { status, data } = await api.get(`/user/${User._id}`)
+
+      if(status === 202) {
+        notificacoes(data.message, typeMessage.WARNING)
+      }
+
+      if(status === 200) {
+        setUser({ ...data })
+      }
+
+    } catch(error) {
+      console.error(error)
+      notificacoes('Falha ao buscar o usuário!', typeMessage.ERROR)
+    }
+  }
+
+  const notificacoes = (message, variant) => {
+    enqueueSnackbar(message, {
+      variant, // success, error, info, warning...
+      anchorOrigin: {
+        vertical: 'top',
+        horizontal: 'center',
+      }, // Localização em que a mensagem irá aparecer...
+      action: (
+        <button className="btn btn-default text-light" onClick={() => closeSnackbar() }>
+          <FontAwesomeIcon icon={faWindowClose} />
+        </button>
+      ),
+    });
+  }
+  
   return (
     <div className="dash-container">
       <div className="ml-2 mr-2">
-        <div className="user mb-3">
-          <img src={user.image_url} className="image ml-3 mr-3" alt={user.username} />
-          <label className="name">{`${user.name} ${user.surname}`}</label>
+        <div className="user">
+          <img src={user.image_url} className="image" alt={user.username} />
+          <label className="name mt-3">{`${user.name} ${user.surname}`}</label>
         </div>
         <button
           type="button"
